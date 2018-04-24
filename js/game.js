30abcfe30ba9674e29a418a20bfe97084557a648
eric98 <ergare.17@gmail.com>
2018-04-24 20:58:16 +0200

    Sync

--- js/game.js
+++ js/game.js
@@ -1,23 +1,79 @@
 var level1 = {
   preload: function () {
-    console.log('PRELOAD')
-
     game.load.image('wall','assets/wall.png')
     game.load.image('ground','assets/ground.png')
+    game.load.image('coin','assets/coin.png')
+
+    game.load.spritesheet('player','assets/player.png', 28, 22)
+
+    game.load.audio('coin', ['assets/coin.wav','assets/coin.mp3'])
   },
   create: function () {
-    console.log('create')
+    this.cursor = game.input.keyboard.createCursorKeys()
+
+    this.coinSound = game.add.audio('coin',0.1)
+
+    this.loadLevel()
+
+    this.player = game.add.sprite(250,50,'player')
+
+    game.physics.arcade.enable(this.player)
+
+    this.player.body.gravity.y = 600
+    this.player.animations.add('idle',[3,4,5,4],5,true)
   },
-  upload: function () {
-    // console.log('upload')
+  update: function () {
+    game.physics.arcade.collide(this.player,this.level)
+    game.physics.arcade.overlap(this.player,this.coins, this.takeCoin, null, this)
+
+    this.inputs()
+
+    this.player.animations.play('idle')
   },
   render: function () {
-    // console.log('render')
+
+  },
+  takeCoin(a,b) {
+
+    game.add.tween(b.scale).to({x:0},150).start()
+    game.add.tween(b).to({y:50},150).start()
+
+    this.coinSound.play()
+
   },
   loadLevel(){
-    game.add.sprite(90,200/2-50,'wall')
-    game.add.sprite(390,200/2-50,'wall')
-    game.add.sprite(500/2-160,200/2+30,'ground')
+    this.level = game.add.group()
+    this.level.enableBody = true
+
+    game.add.sprite(90,200/2-50,'wall',0,this.level)
+    game.add.sprite(390,200/2-50,'wall',0,this.level)
+    game.add.sprite(500/2-160,200/2+30,'ground',0,this.level)
+    this.level.setAll('body.immovable',true)
+
+    this.addcoins()
+  },
+  addcoins: function () {
+    this.coins = game.add.group()
+    this.coins.enableBody = true
+
+    game.add.sprite(140,110,'coin',0,this.coins)
+    game.add.sprite(170,110,'coin',0,this.coins)
+    game.add.sprite(200,110,'coin',0,this.coins)
+  },
+  inputs: function () {
+    if (this.cursor.left.isDown) {
+      this.player.body.velocity.x = -200
+      this.player.frame = 2
+    } else if (this.cursor.right.isDown) {
+      this.player.body.velocity.x = 200
+      this.player.frame = 1
+    } else {
+      this.player.body.velocity.x = 0
+    }
+
+    if (this.cursor.up.isDown && this.player.body.touching.down) {
+      this.player.body.velocity.y = -200
+    }
   }
 }
 
